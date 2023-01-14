1a1ac54c7e503ba0d36cd8bd16d11f359258d47a
Madis Luik <madis.luik@eklips.se>
2023-01-14 15:43:58 +0200

    updates

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <div>
-        <span>Hello</span>
-    </div>
+      <>
+          <div className="grid gap-3">
+              <p className="flex flex-wrap gap-3"><span className="font-bold text-5xl">Kodulehed</span><span className="font-semibold text-4xl">E-poed</span></p>
+              <p className="flex flex-wrap gap-3"><span className="font-semibold text-sm self-end">Haldusteenus</span><span className="font-bold">Sisu tootmine</span><span className="text-2xl">Liidestamine</span><span className="font-bold">Disain</span></p>
+              <p className="flex flex-wrap gap-1 text-sm text-blue-100">Tehnoloogiad: <span>Wordpress/Woocommerce</span><span>PHP/JS</span><span>React/Next.js</span><span>Bootstrap/Tailwind</span></p>
+          </div>
+          <p className="mt-4 text-red-200 hover:text-red-100 transition-colors">Võta ühendust: <a className="font-semibold" href="mailto:info@mehh.ee">info@mehh.ee</a></p>
+      </>
   )
 }
--- components/carousel.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CarouselModule = () => {
-    return (
-        <div>Carousel</div>
-    );
-};
-
-export default CarouselModule;
