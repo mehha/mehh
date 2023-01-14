7c5f32b94ddf1877bcf76ebd56757f59d666fc17
Madis Luik <madis.luik@eklips.se>
2023-01-14 14:56:30 +0200

    Initial commit

---
+++ components/carousel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CarouselModule = () => {
+    return (
+        <div>Carousel</div>
+    );
+};
+
+export default CarouselModule;
---
+++ utils/slugs.js
@@ -0,0 +1,10 @@
+// export const customPagesSlugs = (uri) => {
+//     const pagesToExclude = [
+//         '/',
+//         'category'
+//     ]
+//
+//     return pagesToExclude.includes(uri)
+// }
+
+export const customPagesSlugs = ['/'];
