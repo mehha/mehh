4b5e211110d494886e66697adc1adf7aff4167d7
Madis Luik <madis.luik@eklips.se>
2023-06-07 13:40:56 +0300

    analytics

--- app/[lang]/layout.js
+++ app/[lang]/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import React from "react";
 import {Background} from "../../components/svg/svgComponents";
+import Script from "next/script";
 
 async function RootLayout({children}) {
     return (
@@ -14,6 +15,21 @@ async function RootLayout({children}) {
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
             <link href="https://fonts.googleapis.com/css2?family=Anton&family=DM+Sans:wght@400;700&display=swap" rel="stylesheet" />
         </head>
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-4M4MNV3S4F"/>
+        <Script
+          id='google-analytics'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4M4MNV3S4F', {
+                page_path: window.location.pathname,
+              });
+            `,
+            }}
+        />
         <body className="h-full flex">
             <main className="flex xl:items-center flex-1 relative">
                 <div className='max-w-7xl px-4 py-10 xl:px-32 flex-1 z-10'>
