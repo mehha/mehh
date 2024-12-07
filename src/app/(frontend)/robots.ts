46f3e054a5b15f1fd8b540ae18b6cf4e972c180c
Madis Luik <madis@eklips.se>
2024-12-07 17:47:24 +0200

    updates

--- src/app/(frontend)/robots.ts
+++ src/app/(frontend)/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: process.env.NEXT_PUBLIC_SERVER_URL+'/sitemap.xml',
+    sitemap: process.env.NEXT_PUBLIC_SERVER_URL+'/sitemap-4usi79',
   }
 }
--- src/app/(frontend)/sitemap.ts
+++ src/app/(frontend)/sitemap.ts
@@ -2,35 +2,47 @@ import type { MetadataRoute } from 'next'
 import { getPayloadHMR } from '@payloadcms/next/utilities'
 import configPromise from '@payload-config'
 
-export default async function sitemap() {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const payload = await getPayloadHMR({ config: configPromise })
-  const pages = await payload.find({
+  const { docs: pages } = await payload.find({
     collection: 'pages',
+    limit: 0,
   })
 
-  const sitemapEntries = [];
+  const { docs: posts } = await payload.find({
+    collection: 'posts',
+    limit: 0,
+  })
+
+  const sitemap: MetadataRoute.Sitemap = []
   const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL
 
-  console.log('pages', pages)
+  for (const page of pages) {
+    sitemap.push({
+      changeFrequency: 'weekly',
+      lastModified: page.updatedAt,
+      priority: 1,
+      url: `${baseUrl}/${page.slug}`,
+    })
+  }
+
+  for (const post of posts) {
+    sitemap.push({
+      changeFrequency: 'weekly',
+      lastModified: post.updatedAt,
+      priority: 1,
+      url: `${baseUrl}/posts/${post.slug}`,
+    })
+  }
 
   return [
+    ...sitemap,
     {
-      url: 'https://acme.com',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },
-    {
-      url: 'https://acme.com/about',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://acme.com/blog',
+      url: baseUrl,
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 0.5,
+      priority: 1,
     },
+
   ]
 }
